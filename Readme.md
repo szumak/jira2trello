6b72cd7445117629e4a13a81361cc3299153c847
Marek Knappe <marek.knappe@gmail.com>
2021-02-16 18:00:43 +1000

    Added possibility to map statuses

--- Readme.md
+++ Readme.md
@@ -1,8 +1,12 @@
 # jira2trello
 
+# Info
 When invoke without parameters script will search in JIRA for open tickets and
 will create corresponding card in Trello
 
+It will move jira issues to correct cards according to status_mapping
+
+# Alternate run
 Alternatively you can run the script with the card title as an argument.
 After you hit the enter script will allow you to provide a card description.
 Line with a dot '.' only will tell the script that we finished.
--- add_trello_cards.py
+++ add_trello_cards.py
@@ -6,9 +6,10 @@ from jira import JIRA
 import sys
 import re
 import json
+from pprint import pprint
 
 ###
-credentials_file = ""
+credentials_file = "config.ini"
 
 
 with open(credentials_file, 'r') as f:
@@ -32,6 +33,23 @@ def add_trello_card( jira_issue, name ):
     }
     response = requests.request(
        "POST",
+       trello_url,
+       params=query
+    )
+    #print(response.text)
+
+def move_trello_card( card_id, list_id ):
+    print("Moving %s to %s" % (card_id,list_id))
+ 
+    trello_url = config['trello']['url'] + "cards/" + card_id
+
+    query = {
+       'key':      config['trello']['key'],
+       'token':    config['trello']['token'],
+       'idList':   list_id
+    }
+    response = requests.request(
+       "PUT",
        trello_url,
        params=query
     )
@@ -70,7 +88,6 @@ def get_trello_cards():
     )
     return json.loads(response.text)
 
-
 #
 #  START
 #
@@ -85,13 +102,42 @@ if (len( sys.argv )  > 1) :
         description = description + line
     add_fast_card( subject, description )
 
-cards_names = []
+cards_names = {}
 for card in get_trello_cards():
-    cards_names.append( card['name'] )
+    cards_names[str(card['name'])] = card
 
-for issue in get_jira_issues():
+    #print("Name: %s %s" % (card['name'],card['id']))
+
+
+issues_list = get_jira_issues()
+for issue in issues_list:
     #print('adding {}'.format(issue.key) )
-    res = [i for i in cards_names if issue.key in i]
-    if len(res) == 0:
+    #print(issue.fields.status)
+    move_to=config['trello']['status_mapping'][str(issue.fields.status)]
+
+    res=0
+
+    for key in cards_names:
+        if issue.key in key:
+            res=1
+            if cards_names[key]['idList'] != move_to:
+                move_trello_card(cards_names[key]['id'],move_to)
+
+    if res == 0:
         print("adding card for {} - {}".format(issue.key, issue.fields.summary))
         add_trello_card( issue.key, '{}: {}'.format(issue.key, issue.fields.summary) )
+
+
+
+for card in cards_names:
+    sys_ticket=card.split(':')[0]
+    move_to=config['trello']['status_mapping']['Closed']
+    if re.match("[A-Z]+(?!-?[a-zA-Z]{1,10})", sys_ticket):
+        res=0
+        for issue in issues_list:
+            if sys_ticket == issue.key:
+                res=1
+        if res == 0:
+            if cards_names[card]['idList'] != move_to:
+                    move_trello_card(cards_names[card]['id'],move_to)
+        
